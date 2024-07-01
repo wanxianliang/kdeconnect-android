ac439081cb06d48368fd93ce3b92f0f17e386dfc
wanxianliang <wanxianliang@ym.im>
2024-07-01 19:21:38 +0800

    sync

---
+++ modify.sh
@@ -0,0 +1,2 @@
+查看github 上的ahead commits:
+https://github.com/KDE/kdeconnect-android/commit/290da70dff698764e2a46eec2e8ca35f9419735f
---
+++ syncCode.sh
@@ -0,0 +1,2 @@
+git remote add upstream https://github.com/KDE/kdeconnect-android.git
+git pull upstream master
